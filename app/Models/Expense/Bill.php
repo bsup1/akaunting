fe00f1b9e0f34cbfe3aba1b2385416f638f7b05c
Hafez Divandari <hafezdivandari@hotmail.com>
2018-04-04 21:07:56 +0430

    Replace Vendor with Contractor

--- app/Models/Expense/Bill.php
+++ app/Models/Expense/Bill.php
@@ -55,7 +55,7 @@ class Bill extends Model
 
     public function vendor()
     {
-        return $this->belongsTo('App\Models\Expense\Vendor');
+        return $this->belongsTo('App\Models\Contractor\Contractor', 'vendor_id');
     }
 
     public function currency()
